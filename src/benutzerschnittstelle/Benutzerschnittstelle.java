3a94d3950f4a4692319440dcf5a4c9aeb83b600c
Julian Schumacher <Jules.media@outlook.de>
2022-10-26 10:23:51 +0200

    Implement Benutzerschnittstelle

--- src/benutzerschnittstelle/Benutzerschnittstelle.java
+++ src/benutzerschnittstelle/Benutzerschnittstelle.java
@@ -1,4 +1,33 @@
 package benutzerschnittstelle;
 
+import datenspeicherung.ProduktDaten;
+
 public final class Benutzerschnittstelle {
+
+    public Benutzerschnittstelle() {
+    }
+
+    private void geklicktAendern() {
+
+    }
+
+    private void geklicktHinzufuegen() {
+
+    }
+
+    private void geklicktLoeschen() {
+
+    }
+
+    private void gecklicktProdukt() {
+
+    }
+
+    public void zeigeMeldung(String meldung) {
+
+    }
+
+    public void zeigeProdukte(ProduktDaten[] daten) {
+
+    }
 }
